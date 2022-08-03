9a8d351fb9c2814789dadd73bcd763c4714db141
Dolapo Alli <dolapoalli@Dolapos-MBP.attlocal.net>
2022-08-03 00:16:41 -0400

    installed redux persist

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store , {persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 
@@ -12,8 +13,10 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import createSagaMiddleware from "@redux-saga/core";
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
-
 import rootReducer from "./rootReducer";
 import rootSaga from './postSagas';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -12,13 +13,20 @@ if(process.env.NODE_ENV === "development"){
     middlewares.push(logger)
 }
 
+const persistConfig = {
+    key: "persist-Key",
+    storage
+}
+
+const rootPersistReducer = persistReducer(persistConfig, rootReducer )
 
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
-
+const store = createStore(rootPersistReducer, applyMiddleware(...middlewares))
+const persistor = persistStore(store)
 
 sagaMiddleware.run(rootSaga);
 
 
 
 export default store;
+export {persistor}
